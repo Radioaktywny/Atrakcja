bcc5c40c365caee6a8c4e45de0c22cfcedd9d201
Radioaktywny <markowitek@gmail.com>
2015-12-07 13:38:51 +0100

    Nowy plik

---
+++ src/com/example/projekt_atrakcja/Logowanie.java
@@ -0,0 +1,14 @@
+package com.example.projekt_atrakcja;
+
+public class Logowanie 
+{
+    private int zmienna;
+    public Logowanie()
+    {
+        zmienna=0;
+    }
+    public int zmienna()
+    {
+        return zmienna;
+    }
+}
