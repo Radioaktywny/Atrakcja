d2439bcc03b51868aba56529dc721452676e0e1d
Radioaktywny <markowitek@gmail.com>
2015-12-14 18:05:31 +0100

    Dodanie klasy User posiadajacej na razie haslo i login

---
+++ src/logowanie/User.java
@@ -0,0 +1,23 @@
+package logowanie;
+
+public class User 
+
+{
+    private final String password;
+    private final String login;
+            public User(String password, String login)
+            {
+                this.password=password;
+                this.login=login;
+            }
+            public String getPassword() 
+            {
+                return password;
+            }
+            public String getLogin() 
+            {
+                return login;
+            }
+            
+            
+}
